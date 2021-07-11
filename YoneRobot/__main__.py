bb635e9c9691b4c38613a2bc808e1a656a52cca4
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-11 10:49:42 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -356,9 +356,9 @@ def yone_about_callback(update, context):
                  \n❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  \n❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  \n❍ I check for admins' permissions before executing any command and more stuffs
-                 \n❍ yone's licensed under the GNU General Public License v3.0_
+                 \n❍ _Emilia is licensed under the GNU General Public License v3.0_
                  ).
-                 \n\nIf you have any question about yone, let us know at .""",
+                 \n\nIf you have any question about Emilia, let us know.""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
