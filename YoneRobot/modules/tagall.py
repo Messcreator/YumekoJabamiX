c26e570ec98b39b566ffd2ad54593a1125ea6c22
IzumiCypherX <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-18 12:03:17 +0530

    Update tagall.py

--- YoneRobot/modules/tagall.py
+++ YoneRobot/modules/tagall.py
@@ -13,7 +13,7 @@ from YoneRobot.pyrogramee.pluginshelper import admins_only, get_text
 from YoneRobot import pbot
 
 
-@pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
+@pgram.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
     await message.reply("`Processing.....`")
