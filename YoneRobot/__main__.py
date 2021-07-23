0d640f49ce76e540cf33d1cac7c8c999abf0af54
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-07-23 21:28:10 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -684,7 +684,8 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Emilia Is Up Again [😀](https://telegra.ph/file/4e87c85b6bbae331dd78f.jpg), parse_mode=ParseMode.MARKDOWN")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Emilia Is Up Again [😀](https://telegra.ph/file/4e87c85b6bbae331dd78f.jpg)")
+            parse_mode=ParseMode.MARKDOWN,
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
